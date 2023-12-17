75a3fe2927b1ac7eb181a3f09b0cad746f208dac
GiorioGovano <154093657+GiorioGovano@users.noreply.github.com>
2023-12-17 22:56:24 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+﻿let Username = ""
+
+while (true)
+{
+    Username = prompt("Введи своє ім'я")
+    if (Username) {
+        if (Username.length) break;
+    }
+    
+}
+document.getElementById("Inputluck").innerHTML = `Вітаю, ${Username}, на моїй сторінці!`
+
+/*console.log(Username)*/
+alert(`Вітаю, ${Username}, на моїй сторінці!`)
